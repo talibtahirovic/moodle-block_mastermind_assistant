44c0f1f954ff244df581daba13e50722c14ddea0
Talib Tahirovic <talibtahirovic@gmail.com>
2026-05-07 11:35:40 +0200

    lang: install nudge + course nav strings (en, de); bump v3.6.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_mastermind_assistant';
-$plugin->version = 2026050602;
+$plugin->version = 2026050700;
 $plugin->requires = 2024042200; // Moodle 4.4 or later.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v3.5.0';
+$plugin->release = 'v3.6.0';
